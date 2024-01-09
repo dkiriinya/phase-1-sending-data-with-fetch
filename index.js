fec5b51fd5de4d51b5a3aa39317f54ce51d46ddb
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-09 11:31:52 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+            document.body.innerHTML = data.id;
+        })
+        .catch(error => {
+           document.body.innerHTML = error.message
+        });
+}
+
+submitData('dgk_tec', 'dkiriinya06sc@gmail.com');
